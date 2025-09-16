71fb40ba1a6460ca170a92927adba68634171ab8
prchazz78-tech <prchazz78@gmail.com>
2025-09-16 17:38:25 -0400

    Add files via upload

---
+++ src/TestApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TestApp() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Travel Tiles Test</h1>
+      <p>React is working!</p>
+      <button onClick={() => alert('Button clicked!')}>Test Button</button>
+    </div>
+  )
+}
+
+export default TestApp
